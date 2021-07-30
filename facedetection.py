3294431e655e9bb98c29fd2393b373321c3cc990
mhdshjn <mohammedshajin1997@gmail.com>
2021-07-30 12:33:59 +0530

    Comments Added

--- facedetection.py
+++ facedetection.py
@@ -1,15 +1,21 @@
 import cv2
 
+# Create the cascade
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
+# Read the image
 img = cv2.imread('faces.jpg')
 
+# Grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+# Detect faces
 faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
+# Mark the faces with rectangles
 for (x, y, w, h) in faces:
     cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
+# Show the image(output)
 cv2.imshow('img', img)
 cv2.waitKey()
